260a16089c7371edc76f13182c9955b80b03b881
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2025-01-29 15:50:01 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+  int num,fact=1;
+  printf("Enter a number : ");
+  scanf("%d",&num);
+  while (num>0){
+  
+    fact = fact * num;
+    num--;
+  }
+  printf("%d",fact);
+    return 0;
+  }  
