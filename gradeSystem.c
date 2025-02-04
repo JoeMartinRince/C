be9b3e5a0a56ea3477222e2541e81afb3edad9f9
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2025-02-05 00:25:18 +0530

    gradeSystem.c

---
+++ gradeSystem.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char grade;
+
+    printf("Enter the grade: ");
+    scanf("%s", &grade);
+    switch(grade){
+     case 'E':
+      printf("EXCELLENT");
+      break;
+     case 'V':
+            printf("VERY GOOD");
+            break;
+     case 'G':
+            printf("GOOD");
+            break;
+     case 'A':
+            printf("AVERAGE");
+            break;
+     case 'F':
+            printf("FAIL");
+            break;
+     default:
+      printf("Wrong Input");
+     }
+return 0;
